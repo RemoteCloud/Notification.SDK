9d685851556eedafbdd95553aade9475f0d73743
Oskars Zaumanis <oza@maranics.com>
2022-09-08 16:51:00 +0300

    MNA-43 - added push notification subscription registration example; Added VAPID retrieval example.

--- Notifications.SDK.Demo/Program.cs
+++ Notifications.SDK.Demo/Program.cs
@@ -1,25 +1,42 @@
 ﻿using Maranics.AppStore.SDK;
 using Maranics.AppStore.SDK.Interfaces;
 using Maranics.Notifications.SDK;
+using Microsoft.AspNetCore.Mvc;
 using Notifications.SDK.Demo;
 
-var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.Development.json", false, true).AddEnvironmentVariables().Build();
+var builder = WebApplication.CreateBuilder();
+builder.Configuration.AddJsonFile("appsettings.Development.json", false, true).AddEnvironmentVariables();
+builder.Services.ConfigureAppStore(builder.Configuration);
+builder.Services.AddNotifications(builder.Configuration, serviceProvider =>
+{
+    var tokenProvider = serviceProvider.GetService<IAccessTokenProvider>();
+    return tokenProvider!.GetTokenAsync();
+});
+builder.Services.AddHostedService<NotificationListenerService>();
+builder.Services.AddHostedService<NotificationSenderService>();
+builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
-using IHost host = Host.CreateDefaultBuilder()
-    .ConfigureServices((_, services) =>
+var app = builder.Build();
+const string tenant = "Development";
+app.Urls.Add("http://localhost:3000");
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapGet("/subscriptions/vapid", ([FromServices] IPushNotificationSubscriptionsManager pushNotificationSubscriptionsManager) => { return pushNotificationSubscriptionsManager.GetVapidPublicKey(tenant); });
+app.MapPost("/subscriptions", (Subscription subscription, [FromServices] IPushNotificationSubscriptionsManager pushNotificationSubscriptionsManager) =>
+{
+    var pushNotificationSubscription = new PushNotificationSubscription()
     {
-        services.ConfigureAppStore(configuration);
-        services.AddNotifications(configuration, serviceProvider =>
-        {
-            var tokenProvider = serviceProvider.GetService<IAccessTokenProvider>();
-            return tokenProvider!.GetTokenAsync();
-        });
-        services.AddHostedService<NotificationListenerService>();
-        services.AddHostedService<NotificationSenderService>();
-    })
-    .ConfigureLogging((context, config)
-        => config.AddConfiguration(configuration.GetSection("Logging")))
-    .Build();
+        Endpoint = subscription.Endpoint,
+        PublicKey = subscription.PublicKey,
+        AuthKey = subscription.AuthKey,
+        ApplicationServerKey = subscription.Vapid,
+        Scope = subscription.AppName,
+        UserId = subscription.UserId
+    };
+    return pushNotificationSubscriptionsManager.RegisterPushNotificationSubscription(tenant, body: pushNotificationSubscription);
+});
 
-host.Run();
-Console.Read();
+app.Run();
---
+++ Notifications.SDK.Demo/Subscription.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Notifications.SDK.Demo
+{
+    public class Subscription
+    {
+        public string Endpoint { get; set; }
+        public string PublicKey { get; set; }
+
+        public string AuthKey { get; set; }
+
+        public string AppName { get; set; }
+
+        public string Vapid { get; set; }
+
+        public Guid UserId { get; set; }
+    }
+}
